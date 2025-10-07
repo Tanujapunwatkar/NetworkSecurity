8157df07695bc6cdec37fe6a116302cb084a8ec4
Tanujapunwatkar <tanujapunwatkar@gmail.com>
2025-10-07 16:51:46 +0530

    Model Training

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,6 +14,9 @@ TEST_FILE_NAME : str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
+SAVED_MODEL_DIR = os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 """ Data Ingestion related constant start with Data_ingestion var name """
 DATA_INGESTION_COLLECTION_NAME : str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME : str = "Tanuja"
@@ -46,3 +49,13 @@ DATA_TRANSFORAMTION_IMPUTER_PARAMS: dict = {
     "n_neighbors": 3,
     "weights": "uniform",
 }
+
+DATA_TRANSFORAMTION_TRAIN_FILE_PATH: str = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+""" Model Trainer ralted constant start with mode trainer var name """
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR : str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITING_UNDER_FITTING_THRESHOLD: float = 0.05
